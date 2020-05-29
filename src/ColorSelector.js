59a5ec99a00a91804e23c8b70ca55d3212d2f997
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2020-05-29 18:28:52 -0400

    Done.

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -4,7 +4,10 @@ export default class ColorSelector extends Component {
 
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      // send actions up to parent componment
+      // Clicking on particular div inside ColorSelector should now set state in Matrix
+      let callback = () => this.props.setSelectedColor(str)
+      return <div onClick={callback} key={idx} className="color-swatch" style={{backgroundColor: str}}/>
     })
   )
 
